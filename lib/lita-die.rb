16e539ff8b38ddecd2bf88ff6bc13d380f11b3b3
Christian Höltje <choltje@us.ibm.com>
2015-06-19 15:40:23 -0400

    rubocop cleanup

--- lib/lita-die.rb
+++ lib/lita-die.rb
@@ -1,12 +1,12 @@
-require "lita"
+require 'lita'
 
 Lita.load_locales Dir[File.expand_path(
-  File.join("..", "..", "locales", "*.yml"), __FILE__
+  File.join('..', '..', 'locales', '*.yml'), __FILE__
 )]
 
-require "lita/handlers/die"
+require 'lita/handlers/die'
 
 Lita::Handlers::Die.template_root File.expand_path(
-  File.join("..", "..", "templates"),
- __FILE__
+  File.join('..', '..', 'templates'),
+  __FILE__
 )
